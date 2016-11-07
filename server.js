43414564caec2e34c48f68a5a97cefd28704e03a
Le Ba Quoc Huynh <huynhlbq@gmail.com>
2016-11-07 11:08:50 +0700

    ready to deploy

--- server.js
+++ server.js
@@ -26,7 +26,13 @@ app.use(stylus.middleware(
 
 app.use(express.static(__dirname + '/public'));
 
-mongoose.connect('mongodb://localhost/multivision');
+if(env === 'development') {
+    mongoose.connect('mongodb://localhost/multivision');
+} else {
+    mongoose.connect('mongodb://hunter:Hunter123@ds143707.mlab.com:43707/multivision');
+}
+//mongoose.connect('mongodb://localhost/multivision');
+//mongoose.connect('mongodb://hunter:Hunter123@ds143707.mlab.com:43707/multivision');
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error ...'));
 db.once('open', function callback() {
@@ -50,6 +56,6 @@ app.get('*', function(req, res) {
     });
 });
 
-var port = 3030;
+var port = process.env.PORT || 3030;
 app.listen(port);
 console.log('Listening on port ' + port + '...');
